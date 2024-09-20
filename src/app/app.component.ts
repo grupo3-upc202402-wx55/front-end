e5e8630067c8301445321db71e9a56e2eef73663
Hardie Alfonso Holguin Gamarra <u202220250@upc.edu.pe>
2024-09-20 15:53:34 -0500

    feat: improve

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
 import {MatToolbar} from "@angular/material/toolbar";
+import {MatAnchor} from "@angular/material/button";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbar],
+  imports: [RouterOutlet, MatToolbar, MatAnchor, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'front-end';
+  title = 'learning-center';
+  protected options = [
+    { path: '/stock', title: 'Inventory Manegement'},
+    {  path: '/learning/courses', title: 'Courses'},
+    { path: '/about', title: 'Shrinkage Control'},
+    { path: '/Billing', title: 'Billing'},
+    { path: '/Orders ', title: 'Orders and Table'},
+    { path: '/Report', title: 'Report and Statistics'},
+    { path: '/Profile', title: 'Profile'}
+
+  ]
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {StockManagementComponent} from "./re-grill/pages/stock-management/stock-management.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'stock', component: StockManagementComponent},
+];
